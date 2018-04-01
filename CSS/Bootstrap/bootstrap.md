de58431e42d463c51ac4bffa94eb277ea3bc72f1
Adam Crowe <adam@adamcrowe.com>
2018-04-01 15:49:06 +0100

    Minor updates

---
+++ CSS/Bootstrap/bootstrap.md
@@ -0,0 +1,2 @@
+* [Bootstrap](https://getbootstrap.com)
+* [Table: Differences_between_bootstrap_3_and_bootstrap_4](https://www.quackit.com/bootstrap/bootstrap_4/differences_between_bootstrap_3_and_bootstrap_4.cfm)
---
+++ JS/patterns.js
@@ -0,0 +1,4 @@
+// http://jargon.js.org/_glossary/DESIGN_PATTERNS.md
+// http://www.dofactory.com/javascript/design-patterns
+// http://shichuan.github.io/javascript-patterns
+// https://addyosmani.com/resources/essentialjsdesignpatterns/book
