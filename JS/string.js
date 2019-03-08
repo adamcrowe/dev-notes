6351d0569bea9444d531d8953bb6b30ae64abbeb
Adam Crowe <adam.crowe@setl.io>
2019-03-08 12:39:10 +0000

    Minor updates

--- JS/string.js
+++ JS/string.js
@@ -2,55 +2,55 @@
 
 // !! Multi-lines
 
-// new line character 
-console.log('string text line 1\n\ 
-string text line 2'); 
-// "string text line 1 
-// string text line 2" 
+// new line character
+console.log('string text line 1\n\
+string text line 2');
+// "string text line 1
+// string text line 2"
 
-// template literals 
-console.log(`string text line 1 
-string text line 2`); 
-// "string text line 1 
+// template literals
+console.log(`string text line 1
+string text line 2`);
+// "string text line 1
 // string text line 2"
 
 // !! Embedded expressions
 
-// concatenation 
-var a = 5; 
-var b = 10; 
-console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.'); 
-// "Fifteen is 15 and 
-// not 20." 
+// concatenation
+var a = 5;
+var b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+// "Fifteen is 15 and
+// not 20."
 
-// template literals 
-var a = 5; 
-var b = 10; 
-console.log(`Fifteen is ${a + b} and\nnot ${2 * a + b}.`); 
-// "Fifteen is 15 and 
+// template literals
+var a = 5;
+var b = 10;
+console.log(`Fifteen is ${a + b} and\nnot ${2 * a + b}.`);
+// "Fifteen is 15 and
 // not 20."
 
 // !! Tagged templates
-var person = 'Mike'; 
-var age = 28; 
+var person = 'Mike';
+var age = 28;
 
-function myTag(strings, personExp, ageExp) { 
-	var str0 = strings[0]; // "that " 
-	var str1 = strings[1]; // " is a " 
-	// There is technically a string after the final expression (in our example), 
-	// but it is empty (""), so disregard. 
-	// var str2 = strings[2]; 
-	var ageStr; 
-	if (ageExp > 99){ 
-		ageStr = 'centenarian'; 
-	} else { 
-		ageStr = 'youngster'; 
-	} 
+function myTag(strings, personExp, ageExp) {
+	var str0 = strings[0]; // "that "
+	var str1 = strings[1]; // " is a "
+	// There is technically a string after the final expression (in our example),
+	// but it is empty (""), so disregard.
+	// var str2 = strings[2];
+	var ageStr;
+	if (ageExp > 99){
+		ageStr = 'centenarian';
+	} else {
+		ageStr = 'youngster';
+	}
 
-	return str0 + personExp + str1 + ageStr; 
-} 
+	return str0 + personExp + str1 + ageStr;
+}
 
-var output = myTag`that ${ person } is a ${ age }`; 
+var output = myTag`that ${ person } is a ${ age }`;
 
-console.log(output); 
+console.log(output);
 // that Mike is a youngster
--- TODO.md
+++ TODO.md
@@ -32,6 +32,7 @@
 * [-] [LET'S DEMYSTIFY JAVASCRIPT'S ‘NEW’ KEYWORD](https://medium.freecodecamp.org/demystifying-javascripts-new-keyword-874df126184c)
 * [-] [NODEJS TUTORIAL](https://www.youtube.com/playlist?list=PLGLfVvz_LVvSpxyVx5XcprEgvhJ1BzruD)
 * [-] [NODE.JS STREAMS: EVERYTHING YOU NEED TO KNOW](https://medium.freecodecamp.org/node-js-streams-everything-you-need-to-know-c9141306be93)
+* [/] [DO YOU WANT A BETTER UNDERSTANDING OF BUFFER IN NODE.JS?](https://medium.freecodecamp.org/do-you-want-a-better-understanding-of-buffer-in-node-js-check-this-out-2e29de2968e8)
 
 # FRONT-END
 * [/] [NG-BOOK (ANGULAR 5)](https://www.ng-book.com/2/)
