3c41e6e3592fd7560c8def0ea5eb51ada56aad1a
Adam Crowe <adam.crowe@setl.io>
2019-03-21 10:19:20 +0000

    Minor updates

---
+++ Testing/unit-testing.js
@@ -0,0 +1,20 @@
+// ! Given-When-Then Format
+// <https://stackoverflow.com/questions/33984182/why-jasmine-is-called-a-bdd-testing-framework-even-if-no-given-when-then-sup/38752262#38752262>
+
+// Use the given-when-then format for precision about what is being tested
+describe("Given a string containing 'foo'", function() {  
+	var someString;
+	beforeEach(function() {
+		someString = "foo";
+	});
+
+   describe("When I append 'bar'", function() {
+		beforeEach(function() {
+			someString += "bar";
+		});
+
+		it("Then the string is 'foobar'", function() {
+			expect(someString).toBe("foobar");
+		});
+	});
+});
