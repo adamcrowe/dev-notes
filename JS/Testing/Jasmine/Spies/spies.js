3de37ed927463bdc20d581ae014dfbbe73ca50d4
Adam Crowe <adam.crowe@setl.io>
2019-08-09 10:43:37 +0100

    Minor updates

--- JS/Testing/Jasmine/Spies/spies.js
+++ JS/Testing/Jasmine/Spies/spies.js
@@ -1,2 +1,11 @@
-// ! Spies specify how code should work 
+// ! Spies specify how code should work
 // <https://jasmine.github.io/2.8/introduction#section-Spies>
+
+// Testing with Mocks & Spies
+// https://codecraft.tv/courses/angular/unit-testing/mocks-and-spies/
+// * # Mocking with fake classes
+// * # Mocking by overriding functions
+// * # Mock by using a real instance with Spy
+// ** By using the spy feature of jasmine we can make any function return anything we want:
+// *** spyOn(service, 'isAuthenticated').and.returnValue(false);
+// *** We create a spy on our service so that if the isAuthenticated function is called it returns false
--- JS/Testing/testing.js
+++ JS/Testing/testing.js
@@ -3,6 +3,15 @@ const { expect } = require('chai');
 const proxyquire = require('proxyquire').noCallThru();
 const { stub } = require('sinon');
 
+// Testing with Mocks & Spies
+// https://codecraft.tv/courses/angular/unit-testing/mocks-and-spies/
+// * # Mocking with fake classes
+// * # Mocking by overriding functions
+// * # Mock by using a real instance with Spy
+// ** By using the spy feature of jasmine we can make any function return anything we want:
+// *** spyOn(service, 'isAuthenticated').and.returnValue(false);
+// *** We create a spy on our service so that if the isAuthenticated function is called it returns false
+
 // TODO:
 // <https://jasmine.github.io/2.8/introduction#section-Jasmine_Clock>
 // <https://jasmine.github.io/2.8/introduction#section-Asynchronous_Support>
